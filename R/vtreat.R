b8dbd59390593c6f973645c251e0813fa558d4e3
John Mount <jmount@win-vector.com>
2015-11-04 11:17:23 -0800

    Remove spurious private function documentation

--- R/vtreat.R
+++ R/vtreat.R
@@ -5,18 +5,12 @@
 NULL
 
 
-#' Return a list of new treated variable names (coresponding to optional original variable names)
-#' @param treatments the treatments slot from a treatmentplan object
-#' @param origVarNames optional restrict to only derived variable originating from these original variables (null is no restriction)
-#' @return list of new treated variable names
-#' @seealso \code{\link{designTreatmentsC}} \code{\link{designTreatmentsN}}
-#' @examples
-#' dTrainN <- data.frame(x=c('a','a','a','a','b','b','b'),
-#'     z=c(1,2,3,4,5,6,7),y=c(0,0,0,1,0,1,1))
-#' dTestN <- data.frame(x=c('a','b','c',NA),
-#'     z=c(10,20,30,NA))
-#' treatmentsN = designTreatmentsN(dTrainN,colnames(dTrainN),'y')
-#' 
+# Return a list of new treated variable names (coresponding to optional original variable names)
+# non-public function, use results in the nmMap slot of your treatment plan.
+# param treatments the treatments slot from a treatmentplan object
+# param origVarNames optional restrict to only derived variable originating from these original variables (null is no restriction)
+# return list of new treated variable names
+# seealso \code{\link{designTreatmentsC}} \code{\link{designTreatmentsN}}
 getNewVarNames <- function(treatments,origVarNames=c()) {
   resCount <- 0
   for(ti in treatments) {
