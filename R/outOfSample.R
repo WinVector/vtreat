6170da31e6f13d3167072f2a6b10990d8388ace7
John Mount <jmount@win-vector.com>
2016-06-12 10:26:42 -0700

    lengthen example

--- R/outOfSample.R
+++ R/outOfSample.R
@@ -88,6 +88,9 @@ problemAppPlan <- function(nRows,nSplits,appPlan,strictCheck) {
   NULL
 }
 
+
+
+
 #' One way holdout, a splitFunction in the sense of vtreat::buildEvalSets.  
 #' Doesn't respect nSplits.
 #' 
@@ -157,8 +160,10 @@ kWayCrossValidation <- function(nRows,nSplits,dframe,y) {
 #' set.seed(23255)
 #' d <- data.frame(y=sin(1:100))
 #' pStrat <- kWayStratifiedY(nrow(d),5,d,d$y)
+#' problemAppPlan(nrow(d),5,pStrat,TRUE)
 #' d$stratGroup <- vtreat::getSplitPlanAppLabels(nrow(d),pStrat)
 #' pSimple <- kWayCrossValidation(nrow(d),5,d,d$y)
+#' problemAppPlan(nrow(d),5,pSimple,TRUE)
 #' d$simpleGroup <- vtreat::getSplitPlanAppLabels(nrow(d),pSimple)
 #' summary(tapply(d$y,d$simpleGroup,mean))
 #' # ggplot(data=d,aes(x=y,color=as.factor(simpleGroup))) + 
@@ -166,6 +171,22 @@ kWayCrossValidation <- function(nRows,nSplits,dframe,y) {
 #' summary(tapply(d$y,d$stratGroup,mean))
 #' # ggplot(data=d,aes(x=y,color=as.factor(stratGroup))) + 
 #' #   geom_density() + ggtitle('y-stratified grouping')
+#' 
+#' 
+#' # # And you can (and should) use your own functions or libraries.
+#' # splitFn <- function(nRows,nSplits,dframe,y) {
+#' #      fullSeq <- seq_len(nRows)
+#' #      part <- caret::createFolds(y=y,k=nSplits)
+#' #      lapply(part,
+#' #             function(appi) { 
+#' #                 list(train=setdiff(fullSeq,appi),app=appi)
+#' #             })
+#' # }
+#' # pCaret <- splitFn(nrow(d),5,d,d$y)
+#' # problemAppPlan(nrow(d),5,pCaret,TRUE)
+#' # d$caretGroup <- vtreat::getSplitPlanAppLabels(nrow(d),pCaret)
+#' # ggplot(data=d,aes(x=y,color=as.factor(caretGroup))) +
+#' #    geom_density() + ggtitle('caret::createFolds grouping')
 #' 
 #' @export
 kWayStratifiedY <- function(nRows,nSplits,dframe,y) {
