5bf5d9e60e9bf019b3e2dd4ba707dd56a3cb4a58
John Mount <jmount@win-vector.com>
2016-12-29 12:08:32 -0800

    add ad-hoc nonlinear transforms

---
+++ R/AdHocTreatments.R
@@ -0,0 +1,149 @@
+
+#' @importFrom stats predict
+NULL
+
+#' Build a monotone transform transform in a chosen direction
+#' 
+#' Depends on stats
+#'
+#' @param x numeric vector input
+#' @param y numeric vector (same lenght as x) target output
+#' @param decreasing logical if TRUE build decreasing instead
+#' @return monotone function mapping x to y
+#' 
+#' @examples
+#' 
+#' d <- data.frame(x=c(3,1,-12,NA,5,NaN))
+#' d$y <- log(d$x)
+#' m1 <- monotoneXformD(d$x,d$y)
+#' m2 <- monotoneXformD(d$x,d$y,decreasing=TRUE)
+#' d$pred1 <- m1(d$x)
+#' d$pred2 <- m2(d$x)
+#' 
+#' @export
+#'
+monotoneXformD <- function(x, y, decreasing=FALSE) {
+  x <- as.numeric(x)
+  y <- as.numeric(y)
+  if(decreasing) {
+    x <- -x
+  }
+  good <- (!is.na(x)) & (!is.na(y))
+  x <- x[good]
+  y <- y[good]
+  ord <- order(x)
+  x <- x[ord]
+  y <- y[ord]
+  m <- stats::isoreg(x,y)
+  xvals <- m$x[m$iKnots]
+  yvals <- m$yf[m$iKnots]
+  rm(list=setdiff(ls(),c('xvals','yvals','decreasing')))
+  function(x) {
+    x <- as.numeric(x)
+    if(decreasing) {
+      x <- -x
+    }
+    nknot <- length(xvals)
+    yhat <- rep(NA_real_,length(x))
+    nonNA <- !is.na(x)
+    if(length(xvals)<=1) {
+      yhat[nonNA] = yvals[[1]]
+    } else {
+      ## step version
+      #lookups <- pmin(length(x),pmax(1,findInterval(x,xvals)))
+      #yhat[nonNA] <- yvals[lookups[nonNA]]
+      ## linearly interpolated version
+      yhat[nonNA] <- stats::approx(x=xvals,y=yvals,
+                                   yleft=yvals[[1]],
+                                   yright=yvals[[nknot]],
+                                   xout=x[nonNA])$y
+    }
+    yhat
+  }
+}
+
+
+#' Build a monotone transform
+#' 
+#' Depends on stats
+#'
+#' @param x numeric vector input
+#' @param y numeric vector (same lenght as x) target output
+#' @return monotone function mapping x to y
+#' 
+#' @examples
+#' 
+#' d <- data.frame(x=c(15,1,-12,NA,5,NaN))
+#' d$y <- -log(d$x)
+#' m <- monotoneXform(d$x,d$y)
+#' d2 <- data.frame(x=-20:20)
+#' d2$pred <- m(d2$x)
+#' # ggplot() + geom_point(data=d,mapping=aes(x=x,y=y)) + 
+#' #  geom_line(data=d2,mapping=aes(x=x,y=pred),color='blue')
+#' 
+#' @export
+#'
+monotoneXform <- function(x, y) {
+  xi <- monotoneXformD(x,y,decreasing=FALSE)
+  ei <- mean((y-xi(x))^2, na.rm= TRUE)
+  if(ei<=0) {
+    return(xi)
+  }
+  xd <- monotoneXformD(x,y,decreasing=TRUE)
+  ed <- mean((y-xd(x))^2, na.rm= TRUE)
+  if(ei<=ed) {
+    return(xi)
+  }
+  return(xd)
+}
+
+
+#' Build a one variable GAM transform
+#' 
+#' Depends on mgcv and stats
+#'
+#' @param x numeric vector input
+#' @param y numeric vector (same lenght as x) target output
+#' @param family mgcv/stats family object specifying the distribution and link to use in fitting 
+#' @return increasing function mapping x to y
+#' 
+#' @examples
+#' 
+#' d <- data.frame(x=0.05*(1:100))
+#' d$y <- sin(d$x)
+#' m <- gamXform(d$x,d$y)
+#' d$pred <- m(d$x)
+#' # ggplot(data=d,mapping=aes(x=x)) + geom_point(aes(y=y)) +
+#' #   geom_line(aes(y=pred),color='blue')
+#' 
+#' d$yCat <- d$y>0.1
+#' mC <- gamXform(d$x, d$yC, family= stats::binomial(link='logit'))
+#' d$predC <- mC(d$x)
+#' # ggplot(data=d,mapping=aes(x=predC,color=yCat)) + geom_density()
+#' 
+#' @export
+#'
+gamXform <- function(x, y, family=stats::gaussian()) {
+  x <- as.numeric(x)
+  y <- as.numeric(y)
+  good <- (!is.na(x)) & (!is.na(y))
+  x <- x[good]
+  y <- y[good]
+  m <- NULL
+  if(requireNamespace('mgcv', quietly = TRUE)) {
+    tryCatch(
+      m <- mgcv::gam(y~s(x), data=data.frame(x=x,y=y), family=family),
+      error = function(e) { NULL })
+  }
+  if(is.null(m)) {
+    m <- glm(y~x, data=data.frame(x=x,y=y), family=family)
+  }
+  rm(list=setdiff(ls(),c('m')))
+  function(x) {
+    x <- as.numeric(x)
+    yhat <- rep(NA_real_,length(x))
+    nonNA <- !is.na(x)
+    yhat[nonNA] <- predict(m, newdata= data.frame(x=x[nonNA]), type='response')
+    yhat
+  }
+}
