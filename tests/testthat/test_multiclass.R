9cf7398f493db25b1bf096f97913f299562a16bf
John Mount <jmount@win-vector.com>
2018-07-21 09:30:29 -0700

    test column names

---
+++ tests/testthat/test_multiclass.R
@@ -0,0 +1,37 @@
+library('vtreat')
+
+context("MulitClass")
+
+test_that("test_multiclass.R: Works As Expected", {
+  # create example data
+  set.seed(326346)
+  sym_bonuses <- rnorm(3)
+  names(sym_bonuses) <- c("a", "b", "c")
+  sym_bonuses3 <- rnorm(3)
+  names(sym_bonuses3) <- as.character(seq_len(length(sym_bonuses3)))
+  n_row <- 1000
+  d <- data.frame(x1 = rnorm(n_row),
+                  x2 = sample(names(sym_bonuses), n_row, replace = TRUE),
+                  x3 = sample(names(sym_bonuses3), n_row, replace = TRUE),
+                  y = "NoInfo",
+                  stringsAsFactors = FALSE)
+  d$y[sym_bonuses[d$x2] > pmax(d$x1, sym_bonuses3[d$x3], runif(n_row))] <- "Large1"
+  d$y[sym_bonuses3[d$x3] > pmax(sym_bonuses[d$x2], d$x1, runif(n_row))] <- "Large2"
+  
+  # define problem
+  vars <- c("x1", "x2", "x3")
+  y_name <- "y"
+  y_levels <- sort(unique(d[[y_name]]))
+  
+  # build the multi-class cross frame and treatments
+  cfe_m <- mkCrossFrameMExperiment(d, vars, y_name)
+  sf <- cfe_m$score_frame
+  cf <- cfe_m$cross_frame
+  prepped <- prepare(cfe_m$treat_m, d)
+  testthat::expect_equal(sort(colnames(prepped)), 
+                         sort(colnames(cf)))
+  testthat::expect_equal(character(0), 
+                         setdiff(sf$varName, colnames(cf)))
+  testthat::expect_equal("y", 
+                         setdiff(colnames(cf), sf$varName))
+})
